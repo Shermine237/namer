0244b9e1be17cbb55d969c7ca68af14b404ef4ab
Charlie Rostant YOSSA <Charlieyossa@gmail.com>
2024-04-02 14:26:25 +0200

    Add folder option

--- lib/folder.dart
+++ lib/folder.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:path/path.dart' as path;
+import 'package:file_picker/file_picker.dart'; // Import pour PickedFile
+import 'dart:io'; // Import pour la classe File
 
 class FolderPage extends StatefulWidget {
   const FolderPage({Key? key}) : super(key: key);
@@ -10,56 +12,104 @@ class FolderPage extends StatefulWidget {
 }
 
 class _FolderPageState extends State<FolderPage> {
-  List<String> _folders = [];
+  List<firebase_storage.Reference> _files = [];
+  List<bool> _selectedItems = [];
 
   @override
   void initState() {
     super.initState();
-    _fetchFolders();
+    _listFiles();
   }
 
-  Future<void> _fetchFolders() async {
-    List<firebase_storage.Reference> allItems = await _listAllItems();
-
-    List<String> folders = [];
-
-    for (var item in allItems) {
-      String folderName = path.dirname(item.fullPath);
-      if (!folders.contains(folderName)) {
-        folders.add(folderName);
-      }
+  Future<void> _listFiles() async {
+    try {
+      firebase_storage.Reference storageRef =
+          firebase_storage.FirebaseStorage.instance.ref('Public');
+      firebase_storage.ListResult result = await storageRef.listAll();
+      setState(() {
+        _files = result.items;
+        _selectedItems = List.generate(result.items.length, (index) => false);
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Une erreur est survenue : $e')),
+      );
     }
+  }
 
+  Future<void> _uploadFile() async {
+    try {
+      final FilePickerResult? pickedFile =
+          await FilePicker.platform.pickFiles(type: FileType.any);
+      if (pickedFile != null) {
+        final File file = File(pickedFile.files.single.path!);
+        String filename = path.basename(file.path);
+        firebase_storage.Reference storageRef =
+            firebase_storage.FirebaseStorage.instance
+                .ref('Public/$filename');
+        await storageRef.putFile(file);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Fichier téléversé avec succès !')),
+        );
+        _listFiles();
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Échec du téléversement du fichier : $e')),
+      );
+    }
+  }
+
+  void _toggleSelection(int index) {
     setState(() {
-      _folders = folders;
+      _selectedItems[index] = !_selectedItems[index];
     });
   }
 
-  Future<List<firebase_storage.Reference>> _listAllItems() async {
-    firebase_storage.ListResult result =
-        await firebase_storage.FirebaseStorage.instance.ref().listAll();
-
-    return result.items;
+  void _deleteSelected() {
+    // Ajoutez ici la logique pour supprimer les fichiers sélectionnés
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Folder List'),
+        title: const Text('Dossier public'),
+        leading: IconButton(
+          onPressed: _deleteSelected,
+          icon: const Icon(Icons.delete),
+          tooltip: 'Supprimer les fichiers sélectionnés',
+        ),
       ),
-      body: ListView.builder(
-        itemCount: _folders.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_folders[index]),
-            onTap: () {
-              // Handle folder tap
-              String folderName = _folders[index];
-              // Do something with the folderName
-            },
-          );
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _files.length,
+              itemBuilder: (context, index) {
+                firebase_storage.Reference fileRef = _files[index];
+                String filename = path.basename(fileRef.fullPath);
+                bool isSelected = _selectedItems[index];
+                return ListTile(
+                  title: Text(filename),
+                  onTap: () => _toggleSelection(index),
+                  tileColor: isSelected ? Colors.blue.withOpacity(0.3) : null,
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              onPressed: () => _uploadFile(),
+              icon: const Icon(Icons.cloud_upload),
+              label: const Text('Téléverser un fichier'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(16.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
